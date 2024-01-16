487f4addbfbc3e387c43391467560884048b0e43
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-16 21:54:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,8 +10,12 @@ app.listen(port, () => {
 // connect to mongodb database using mongoose
 
 const mongoURI =
-  "mongodb+srv://hasinduonline:gkr11388tm@cluster0.ecidohi.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://hasinduonline:6O66S7eC4S2sw3Fo@cluster0.ecidohi.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
-  console.log("The connection with mongod is established");
+mongoose.connect(mongoURI, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.error("Error connecting to MongoDB:", err);
+  } else {
+    console.log("The connection with MongoDB is established");
+  }
 });
