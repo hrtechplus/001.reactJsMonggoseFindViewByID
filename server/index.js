54285972755246c20d0afca19b389a0932bb9a28
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-16 21:56:07 +0530

    Update index.js
    connection established using monogoDB drivers

--- server/index.js
+++ server/index.js
@@ -7,15 +7,16 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-// connect to mongodb database using mongoose
+// Connect to MongoDB database using mongoose
 
 const mongoURI =
-  "mongodb+srv://hasinduonline:6O66S7eC4S2sw3Fo@cluster0.ecidohi.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://hasinduonline:6O66S7eC4S2sw3Fo@cluster0.ecidohi.mongodb.net/your-database-name?retryWrites=true&w=majority";
 
-mongoose.connect(mongoURI, { useNewUrlParser: true }, (err) => {
-  if (err) {
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
-  } else {
-    console.log("The connection with MongoDB is established");
-  }
-});
+  });
