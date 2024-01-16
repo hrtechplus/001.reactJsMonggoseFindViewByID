a670a8611a2362fbf4d52846be06214e8bca3bfb
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-16 22:47:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 const mongoose = require("mongoose");
+const getRouter = require("./routes/get");
 // middleware
 app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
