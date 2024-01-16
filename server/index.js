d08966689b1901dbe71c9ceb4cdb3ed1384fcf0a
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-16 21:49:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-
 const mongoose = require("mongoose");
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
@@ -10,22 +10,8 @@ app.listen(port, () => {
 // connect to mongodb database using mongoose
 
 const mongoURI =
-  "mongodb+srv://hasindu:TqCFHY7JslwNNwns@mern.tz2lnh1.mongodb.net/mern?retryWrites=true&w=majority";
+  "mongodb+srv://hasinduonline:gkr11388tm@cluster0.ecidohi.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(mongoURI, { useNewUrlParser: true }, () => {
   console.log("The connection with mongod is established");
 });
-
-const Schema = mongoose.Schema;
-
-const userSchema = new Schema({
-  name: String,
-  age: Number,
-  email: String,
-});
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
-const mongoose = require("mongoose");
-const db = mongoose.connection;
