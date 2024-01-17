6cfbe31b37c842f88267047ca915e0037de11f00
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-17 22:04:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const core = require("cors");
 const port = process.env.PORT || 8080;
 const mongoose = require("mongoose");
 const getRouter = require("./routes/get");
