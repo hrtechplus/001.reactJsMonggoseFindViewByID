08d0975036836724aff99f4435525eb3eeca70c0
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-18 23:40:45 +0530

    inedx and randomQuote.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const mongoURI =
   "mongodb+srv://hasinduonline:6O66S7eC4S2sw3Fo@cluster0.ecidohi.mongodb.net/MERN_Learn?retryWrites=true&w=majority";
 
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
   })
---
+++ server/routes/Database/quotes.js
@@ -0,0 +1,37 @@
+const quotes = [
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "Innovation distinguishes between a leader and a follower.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+];
+
+function getRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
+
+function getRandomQuoteJson() {
+  const randomQuote = getRandomQuote();
+  return JSON.stringify(randomQuote);
+}
+
+module.exports = getRandomQuoteJson;
