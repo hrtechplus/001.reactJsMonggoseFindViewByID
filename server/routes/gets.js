2e645bfae0221209b029e6bfb0857b43e0caba09
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-18 23:52:00 +0530

    Update gets.js

--- server/routes/gets.js
+++ server/routes/gets.js
@@ -1,15 +1,13 @@
-const fs = require("fs");
+const express = require("express");
+const router = express.Router();
+const quotes = require("../Database/quotes");
 
-// GET method
-app.get("/api/data", (req, res) => {
-  // Read the contents of the JavaScript file
-  fs.readFile("/path/to/another/file.js", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    } else {
-      // Send the contents of the file as the response
-      res.send(data);
-    }
-  });
+router.get("/", (req, res) => {
+  // Call the function from quotes.js to get the item
+  const item = quotes.getItem();
+
+  // Send the item as the response
+  res.send(item);
 });
+
+module.exports = router;
