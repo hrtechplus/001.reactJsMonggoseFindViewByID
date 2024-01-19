237862077e0748341ea498d7cc79fa50d97d5e52
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-19 13:52:04 +0530

    Update gets.js

--- server/routes/gets.js
+++ server/routes/gets.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const quotes = require("../Database/quotes");
 
+// Change the route path from "/" to "/quotes"
 app.get("/", (req, res) => {
+  const randomQuote = quotes();
+  console.log(randomQuote);
   res.send("Hello World");
 });
 
