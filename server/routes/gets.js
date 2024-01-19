8bc3777c0e14fdcd92ae9a029f9de164b26b88b7
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-19 14:33:44 +0530

    Update gets.js
    problem solved, he problem is , the link file and  imported files are not in the same level of the directory. i'll check that later and confirm.

--- server/routes/gets.js
+++ server/routes/gets.js
@@ -5,8 +5,8 @@ const quotes = require("./quotes");
 // Change the route path from "/" to "/quotes"
 app.get("/", (req, res) => {
   const randomQuote = quotes();
-  console.log(randomQuote);
-  res.send("Hello World");
+
+  res.json(JSON.stringify(randomQuote));
 });
 
 module.exports = app;
