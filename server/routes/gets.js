784e891e93a9682fb89ba49b52c01c13c242301c
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-01-19 11:26:32 +0530

    Update gets.js

--- server/routes/gets.js
+++ server/routes/gets.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const getRandomQuoteJson = require("../Database/quotes");
 
 router.get("/gets", (req, res) => {
+  nf;
   // Call the function from quotes.js to get the JSON data
   const jsonQuote = getRandomQuoteJson();
 
   // Send the JSON data as the response
-  res.send(jsonQuote);
+  // res.send(jsonQuote);
 });
 
 module.exports = router;
